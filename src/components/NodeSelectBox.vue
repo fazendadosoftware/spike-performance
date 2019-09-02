dccb7d03ca09fb715f7fc8fd39da3e53b361272a
Paulo dos Santos <paulo@fazendadosoftware.com>
2019-09-02 09:42:13 -0300

    current state

--- src/components/NodeSelectBox.vue
+++ src/components/NodeSelectBox.vue
@@ -1,12 +1,12 @@
 <template>
   <div>
-    <div class="flex items-center mb-2">
+    <div class="flex items-center mb-3">
       <div class="block relative w-64">
         <select
           v-model="factSheetType"
           :disabled="tree.length > 1"
           class="block appearance-none w-full bg-white border border-gray-400 hover:border-gray-500 px-4 py-2 pr-8 rounded shadow leading-tight focus:outline-none focus:shadow-outline"
-          :class="editable ? 'cursor-pointer' : 'cursor-default'"
+          :class="editable && tree.length === 1 ? 'cursor-pointer' : 'cursor-default'"
           >
           <option
             v-for="(option, idx) in factSheetTypeOptions"
@@ -20,6 +20,7 @@
           <svg class="fill-current h-4 w-4" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20"><path d="M9.293 12.95l.707.707L15.657 8l-1.414-1.414L10 10.828 5.757 6.586 4.343 8z"/></svg>
         </div>
       </div>
+      <div class="flex ml-2 w-24"/>
     </div>
     <div class="flex items-center" v-if="isLastInTree">
       <div class="block relative w-64">
@@ -112,7 +113,7 @@ export default {
       return filteredRelations
     },
     editable () {
-      return this.tree.length === 2 || (this.idx >= (this.tree.length - 2))
+      return this.tree.length === 1 || (this.idx === (this.tree.length - 1))
     },
     factSheetTypeOptions () {
       let options = []
