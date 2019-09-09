c135ae4f0c98f5ca07c94c519870ee063db9e8c4
Paulo dos Santos <paulo@fazendadosoftware.com>
2019-09-09 16:11:30 -0300

    current state

---
+++ src/components/FactsheetNode.vue
@@ -0,0 +1,29 @@
+<template>
+  <div class="absolute bg-transparent rounded border p-1 m-1" :style="nodeStyle">NODE {{node.type}} {{node.label}}</div>
+</template>
+<script>
+export default {
+  name: 'FactsheetNode',
+  props: {
+    node: {
+      type: Object,
+      required: true
+    }
+  },
+  data: () => ({
+    clientDims: { width: 0, height: 0 }
+  }),
+  computed: {
+    nodeStyle () {
+      const { width = 0, height = 0 } = this.clientDims
+      const { x, y } = this.node
+      const style = `left: ${x - (width / 2)}px; top: ${y - (height / 2)}px`
+      return style
+    }
+  },
+  mounted () {
+    const { clientWidth = 0, clientHeight = 0 } = this.$el || {}
+    this.clientDims = { width: clientWidth, height: clientHeight }
+  }
+}
+</script>
--- src/components/RelationshipTree.vue
+++ src/components/RelationshipTree.vue
@@ -1,5 +1,46 @@
 <template>
-  <div class="bg-red-500">
-    Hi from relationship tree
+  <div class="relative">
+    <factsheet-node v-for="node in nodes" :key="node.id" :node="node"/>
   </div>
 </template>
+
+<script>
+import FactsheetNode from './FactsheetNode'
+import { Network } from 'vis-network'
+
+export default {
+  name: 'RelationshipTree',
+  components: { FactsheetNode },
+  data: () => ({
+    factSheet: {},
+    network: undefined,
+    nodes: [],
+    overlayComponents: []
+  }),
+  methods: {
+    getNodeCoords (node) {
+      const { id } = node
+      const pos = this.network.getPositions([id])
+      const coords = this.network.canvasToDOM({ x: pos[id].x, y: pos[id].y })
+      return { ...node, ...coords }
+    }
+  },
+  mounted () {
+    const nodes = [
+      { id: 1, label: '1' },
+      { id: 2, label: '2' }
+    ]
+    const edges = [
+      { from: 1, to: 2, label: '1' },
+      { from: 2, to: 1, label: '2' }
+    ]
+    this.network = new Network(this.$el, { nodes, edges }, {})
+    this.network.on('afterDrawing', () => { this.nodes = nodes.map(this.getNodeCoords) })
+    this.network.on('beforeDrawing', () => { this.nodes = [] })
+  },
+  beforeDestroy () {
+    this.network.off('click')
+    this.network.destroy()
+  }
+}
+</script>
