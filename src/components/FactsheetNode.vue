033459f65e31262724de6e3c3ccc6a726188b228
Paulo dos Santos <paulo@fazendadosoftware.com>
2019-09-11 13:58:13 -0300

    current state

--- src/components/FactsheetNode.vue
+++ src/components/FactsheetNode.vue
@@ -1,5 +1,7 @@
 <template>
-  <div class="absolute bg-transparent rounded border p-1 m-1" :style="nodeStyle">NODE {{node.type}} {{node.label}}</div>
+  <div class="absolute p-1 m-1" :style="nodeStyle">
+    {{node.label}}
+  </div>
 </template>
 <script>
 export default {
@@ -15,15 +17,17 @@ export default {
   }),
   computed: {
     nodeStyle () {
-      const { width = 0, height = 0 } = this.clientDims
+      const clientWidth = 100
+      const clientHeight = 40
+      // const { clientWidth = 150, clientHeight = 50 } = this.$el || {}
       const { x, y } = this.node
-      const style = `left: ${x - (width / 2)}px; top: ${y - (height / 2)}px`
+      const style = `width: ${clientWidth}px; height: ${clientHeight}px; left: ${x - (clientWidth / 2)}px; top: ${y - (clientHeight / 2)}px`
       return style
     }
   },
   mounted () {
-    const { clientWidth = 0, clientHeight = 0 } = this.$el || {}
-    this.clientDims = { width: clientWidth, height: clientHeight }
+    const { offsetWidth = 0, offsetHeight = 0 } = this.$el || {}
+    this.clientDims = { width: offsetWidth, height: offsetHeight }
   }
 }
 </script>
--- src/components/RelationshipTree.vue
+++ src/components/RelationshipTree.vue
@@ -5,6 +5,7 @@
 </template>
 
 <script>
+import { mapGetters } from 'vuex'
 import FactsheetNode from './FactsheetNode'
 import { Network } from 'vis-network'
 
@@ -14,8 +15,8 @@ export default {
   data: () => ({
     factSheet: {},
     network: undefined,
-    nodes: [],
-    overlayComponents: []
+    overlayComponents: [],
+    nodes: []
   }),
   methods: {
     getNodeCoords (node) {
@@ -23,20 +24,77 @@ export default {
       const pos = this.network.getPositions([id])
       const coords = this.network.canvasToDOM({ x: pos[id].x, y: pos[id].y })
       return { ...node, ...coords }
+    },
+    updateNetwork () {
+      const { nodes, edges } = this.treeData
+      if (this.network) this.network.destroy()
+      const options = {
+        autoResize: true,
+        height: `${300}px`,
+        nodes: {
+          shape: 'box',
+          widthConstraint: {
+            minimum: 150,
+            maximum: 150
+          },
+          heightConstraint: {
+            minimum: 40
+          }
+        },
+        edges: {
+          arrows: 'to'
+        },
+        interaction: {
+          dragNodes: false,
+          dragView: false,
+          zoomView: false
+        },
+        layout: {
+          hierarchical: {
+            enabled: true,
+            levelSeparation: 90,
+            direction: 'UD'
+          }
+        },
+        physics: {
+          enabled: false
+        }
+      }
+      this.network = new Network(this.$el, { nodes, edges }, options)
+      this.network.on('afterDrawing', () => { this.nodes = nodes.map(this.getNodeCoords) })
+      this.network.on('beforeDrawing', () => { this.nodes = [] })
+    }
+  },
+  computed: {
+    ...mapGetters({
+      tree: 'performance/tree',
+      viewModel: 'performance/viewModel'
+    }),
+    treeData () {
+      const nodes = [...this.tree]
+        .map(({ relationType, factSheetType, targetFactSheetType }, idx, tree) => {
+          const fsTypeViewModel = this.viewModel[factSheetType] || {}
+          const { color, bgColor } = fsTypeViewModel
+          const font = { color }
+          return { id: relationType, relationType, label: factSheetType, color: bgColor, font, shape: 'box', hidden: false }
+        })
+      const edges = this.tree
+        .map((node, idx, tree) => {
+          const isLastNode = idx === (tree.length - 1)
+          if (!isLastNode) {
+            const nextNode = tree[idx + 1]
+            const from = node.relationType
+            const to = nextNode.relationType
+            return { from, to, arrows: 'to' }
+          }
+        })
+        .filter(link => !!link)
+      const data = { nodes, edges }
+      return data
     }
   },
   mounted () {
-    const nodes = [
-      { id: 1, label: '1' },
-      { id: 2, label: '2' }
-    ]
-    const edges = [
-      { from: 1, to: 2, label: '1' },
-      { from: 2, to: 1, label: '2' }
-    ]
-    this.network = new Network(this.$el, { nodes, edges }, {})
-    this.network.on('afterDrawing', () => { this.nodes = nodes.map(this.getNodeCoords) })
-    this.network.on('beforeDrawing', () => { this.nodes = [] })
+    this.updateNetwork()
   },
   beforeDestroy () {
     this.network.off('click')
